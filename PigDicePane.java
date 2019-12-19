ab0338af2f5ecc74febbfcff2b1a81900cc9b637
Michael George <georgem@wlwv.k12.or.us>
2019-12-19 09:50:18 -0800

    a

--- PigDicePane.java
+++ PigDicePane.java
@@ -15,6 +15,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.control.TextInputControl;
 import javafx.scene.control.TextArea;
 import java.text.DecimalFormat;
+import javafx.scene.layout.VBox;
 
 public class PigDicePane extends Application{
 
@@ -42,8 +43,7 @@ public class PigDicePane extends Application{
 
 public void start(Stage stage){
 
-  Roll.setOnAction(this::updateText);
-  Pass.setOnAction(this::updateText);
+
   //Roll.setOnAction(this::);
 
   P1D1 = new Label("D1: 0");
@@ -57,6 +57,10 @@ public void start(Stage stage){
   Roll = new Button("Roll");
   Pass = new Button("Pass");
   CurrentTurn = new Label("Player 1 Turn");
+  Dice dice1 = new Dice();
+  Dice dice2 = new Dice();
+  Roll.setOnAction(this::RollMeth);
+  Pass.setOnAction(this::changeTurn);
 
   grid.setAlignment(Pos.TOP_LEFT);
   grid.setStyle("-fx-background-color: white");
@@ -84,21 +88,21 @@ public void start(Stage stage){
 
 
 private void updateText(ActionEvent event){
-
   //dice1.getFace().toString(int)
   //Integer.toString(dice1.getFace())
 
-if(p1turn == true){
+if(p1Turn == true){
+
   P1D1.setText(Integer.toString(dice1.getFace()));
-  P1D2.setText(Integer.toString(dice1.getFace()));
+  P1D2.setText(Integer.toString(dice2.getFace()));
 }
-if(p1turn == false){
+if(p1Turn == false){
   P2D1.setText(Integer.toString(dice1.getFace()));
-  P2D2.setText(Integer.toString(dice1.getFace()));
+  P2D2.setText(Integer.toString(dice2.getFace()));
 }
 
-  P1Score.setText("Your Score: " + P1Score);
-  P2Score.setText("Your Score: " + P2Score);
+  P1Score.setText("Your Score: " + p1Score);
+  P2Score.setText("Your Score: " + p2Score);
   if(p1Turn == true){
     P1RoundScore.setText("Round Score: " + roundScore);
     P2RoundScore.setText("Round Score: --");
@@ -110,7 +114,75 @@ if(p1turn == false){
   }
 
 };
-//private void RollMeth(ActionEvent event){
-//  if()
-//}
+private void RollMeth(ActionEvent event){
+ dice1.roll();
+ dice2.roll();
+
+if(dice1.getFace() == 1 || dice2.getFace() == 1){
+  if(dice1.getFace() == 1 && dice2.getFace() == 1){
+    roundScore = 0;
+    P1RoundScore.setText("Round Score: " + roundScore);
+    P2RoundScore.setText("Round Score: " + roundScore);
+    if(p1Turn == true){
+      
+      p1Score = 0;
+      P1Score.setText("Your Score: " + p1Score);
+      p1Turn = !p1Turn;
+    }else{
+      p2Score = 0;
+      P2Score.setText("Your Score: " + p2Score);
+      p1Turn = !p1Turn;
+    }
+
+  }else{
+    roundScore = 0;
+    P1RoundScore.setText("Round Score: " + roundScore);
+    P2RoundScore.setText("Round Score: " + roundScore);
+    p1Turn = !p1Turn;
+  }
+}else{
+  roundScore = roundScore + dice1.getFace() + dice2.getFace();
+  if(p1Turn == true){
+    P1RoundScore.setText("Round Score: " + roundScore);
+  }else{
+    P2RoundScore.setText("Round Score: " + roundScore);
+  }
+}
+}
+
+//P1D1.setText(Integer.toString(dice1.getFace()));
+//P1D2.setText(Integer.toString(dice2.getFace()));
+
+private void changeTurn(ActionEvent event){
+  if(p1Turn == true){
+    p1Score = p1Score + roundScore;
+    P1Score.setText("Your Score: " + p1Score);
+    if(p1Score >= 100){
+      p1Score = 0;
+      p2Score = 0;
+      roundScore = 0;
+      p1Turn = true;
+      P1Score.setText("Player 1 won");
+    }
+    roundScore = 0;
+    P1RoundScore.setText("Round Score: " + roundScore);
+    P2RoundScore.setText("Round Score: " + roundScore);
+    p1Turn = !p1Turn;
+
+  }else{
+    p2Score = p2Score + roundScore;
+    P2Score.setText("Your Score: " + p2Score);
+    if(p2Score >= 100){
+      p1Score = 0;
+      p2Score = 0;
+      roundScore = 0;
+      p1Turn = true;
+      P2Score.setText("Player 2 won");
+    }
+    roundScore = 0;
+    P1RoundScore.setText("Round Score: " + roundScore);
+    P2RoundScore.setText("Round Score: " + roundScore);
+    p1Turn = !p1Turn;
+  }
+}
 };
